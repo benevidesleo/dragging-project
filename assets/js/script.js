6eba9a217fbc7777629c04efe106420da3951175
Leonardo Luis <leoluisbsantos16@gmail.com>
2023-11-05 23:43:00 -0300

    eventos de itens feito!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,41 @@
-let number = 1
+document.querySelectorAll('.item').forEach(item => {
+    item.addEventListener('dragstart', dragStart)
+    item.addEventListener('dragend', dragEnd)
+});
+
+document.querySelectorAll('.area').forEach(area => {
+    area.addEventListener('dragover', dragOver)
+    area.addEventListener('dragleave', dragLeave)
+    area.addEventListener('drop', drop)
+})
+
+
+//FUNCTIONS ITEM
+function dragStart(e) { 
+    e.currentTarget.classList.add('.dragging')
+}
+
+
+function dragEnd(e) {
+    e.currentTarget.classList.remove('.dragging')
+}
+
+// FUNCTIONS AREAS
+
+function dragOver() {
+    e.preventDefault()
+    // quando passar por cima da area
+}
+
+function dragLeave() {
+// quando sair da area 
+}
+
+function drop() {
+// quando soltar o item na area certa
+}
+
+
+
+
+
